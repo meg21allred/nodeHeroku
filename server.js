1fde4eeded9db409892e654ca4f7ce50e9ca332a
meganallred <meg21allred@gmail.com>
2020-11-10 15:03:08 -0500

    adding port

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 
-app.listen(5000);
+//app.listen(5000);
+app.listen(process.env.PORT);
 
 app.get('/', (req, res) => {
     res.send('this is the home page');
