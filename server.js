2a705d20a037916ce8f1c64c450b485edcc305e4
meganallred <meg21allred@gmail.com>
2020-11-10 14:55:17 -0500

    adding express

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+
+app.listen(5000);
+
+app.get('/', (req, res) => {
+    res.send('this is the home page');
+})
